55a05574411769e6f8692a4401d03b415c9b26c5
niklas <jutting.n@gmail.com>
2019-03-26 14:51:41 +0100

    Ajout d'une ébauche de script d'extraction et du lexique des mots

--- extractor.py
+++ extractor.py
@@ -4,10 +4,10 @@ pd.options.display.max_rows = 10
 lx = pd.read_csv('data/lexique.csv', sep='\t')
 
 
-if lx.loc[lx["4_cgram"] == "NOM", :]:
-    print(lx["1_ortho", "2_phon"])
+for x in lx.itertuples():
+    if lx.loc[lx['4_cgram']=="NOM",:]:
+        print(lx["1_ortho"])
 
-
-
+print(x)
 
 
