1508410e516cbdb4848fa74e07ae2c67a09bf7f0
Louis Sharma <louis.sharma2303@gmail.com>
2019-05-13 21:18:53 +0200

    J'ai ajouté le mutually exclusive group qui permet de réguler la richesse de la rime. J'ai pour l'instant que mit les options riche/pauvre et j'ai essayé d'intégrer l'option au moment ou l'on cherche les mots avec le bon nombre de phonèmes. (Je ne peut pas faire tourner le programme sur mon ordi donc dites moi ce qui ne va pas).

--- rimer.py
+++ rimer.py
@@ -7,18 +7,52 @@ import argparse
 with open('data/phonemized_lexicon.json', 'r') as f:
     phonemized_lexicon = json.load(f)
 
-query = input("Quel mot voulez-vous faire rimer?")
-l_rime = int(input("Quelle est la longueur de la rime souhaitée? "))
 
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument("query", help= "Quel mot voulez-vous faire rimer?", type=str)
-#parser.add_argument("rime", help= "Quelle est la nature de la rime recherchée?")
-#parser.add_argument("--gram", help= "Quelle est la classe grammaticalle de la rime recherchée?", type=str)
-#parser.add_argument("--genre", help= "Quelle est le genre de la rime recherchée?", type=str)
+parser = argparse.ArgumentParser()
+parser.add_argument("query", help= "Quel mot voulez-vous faire rimer?", type=str)
+parser.add_argument("--gram", help= "Quelle est la classe grammaticale?", type=str)
+parser.add_argument("--genre", help= "Quelle est le genre de la rime?", type=str)
+group = parser.add_mutually_exclusive_group():
+group.add_argument('-p','--pauvre', help='Rime pauvre', action='store_true',  type=str)
+group.add_argument('-r','--riche', help='Rime riche',  action='store_true', type=str)
+args = parser.parse_args()
+if args.query:
+    v = Voice(lang="fr")
+    phonemes = v.to_phonemes(query)
 
-#args = parser.parse_args()
-#if args.query:
+    pho_query = []
+    for pho in phonemes:
+        if pho.name != "_":
+            pho_query.append(pho.name)
+
+
+    def rime_length(pho_query, pho_list):
+        rime_number = 0
+        min_l = min(len(pho_query), len(pho_list))
+        for i in range(min_l):
+            if pho_query[-(i + 1)] == pho_list[-(i + 1)]:
+                rime_number += 1
+            else:
+                break
+        return rime_number
+
+
+    rimes = []
+
+    for mot, pho_list in phonemized_lexicon.items():
+        if args.riche:
+            if rime_length(pho_query, pho_list) >= 3:
+                rimes.append(mot)
+        elif args.pauvre:
+            if rime_length(pho_query,pho_list) >=1:
+                rimes.append(mot)
+
+
+
+        #if rime_length(pho_query, pho_list) >= l_rime:
+            #rimes.append(mot)
+
 
 
 
