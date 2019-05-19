854b4f8f3ffa9028ef377072d102660f75c4faad
niklas <jutting.n@gmail.com>
2019-05-19 18:14:00 +0200

    progression sur les parsers

--- rimer.py
+++ rimer.py
@@ -4,6 +4,7 @@ import pprint
 import argparse
 
 
+
 with open('data/phonemized_lexicon.json', 'r') as f:
     phonemized_lexicon = json.load(f)
 
@@ -11,62 +12,26 @@ with open('data/phonemized_lexicon.json', 'r') as f:
 
 parser = argparse.ArgumentParser()
 parser.add_argument("query", help= "Quel mot voulez-vous faire rimer?", type=str)
-parser.add_argument("--gram", help= "Quelle est la classe grammaticale?", type=str)
-parser.add_argument("--genre", help= "Quelle est le genre de la rime?", type=str)
+parser.add_argument("--gram", help= "Quelle est la classe grammaticale des rimes recherchées?", type=str)
+parser.add_argument("--genre", help= "Quelle est le genre des rimes recherchées?", type=str)
+parser.add_argument("--nombre", help="Les rimes recherchées doivent-elles être au pluriel ou au singulier?", type=str)
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-p', '--pauvre', help='Rime pauvre', action='store_true',  type=str)
 group.add_argument('-r', '--riche', help='Rime riche',  action='store_true', type=str)
+group.add_argument('-n', '--numérique', help="Rime numérique", action="store_true", type=int)
 
 args = parser.parse_args()
 
 
-if args.query:
-    v = Voice(lang="fr")
-    phonemes = v.to_phonemes(query)
-
-    pho_query = []
-    for pho in phonemes:
-        if pho.name != "_":
-            pho_query.append(pho.name)
-
-
-    def rime_length(pho_query, pho_list):
-        rime_number = 0
-        min_l = min(len(pho_query), len(pho_list))
-        for i in range(min_l):
-            if pho_query[-(i + 1)] == pho_list[-(i + 1)]:
-                rime_number += 1
-            else:
-                break
-        return rime_number
-
-
-    rimes = []
-
-    for mot, pho_list in phonemized_lexicon.items():
-        if args.riche:
-            if rime_length(pho_query, pho_list) >= 3:
-                rimes.append(mot)
-        elif args.pauvre:
-            if rime_length(pho_query,pho_list) >=1:
-                rimes.append(mot)
-
-
-
-        #if rime_length(pho_query, pho_list) >= l_rime:
-            #rimes.append(mot) x
-
-
-
 
 v = Voice(lang="fr")
-phonemes = v.to_phonemes(query)
+phonemes = v.to_phonemes(args.query)
 
 pho_query = []
 for pho in phonemes:
     if pho.name != "_":
         pho_query.append(pho.name)
-        
+
 
 
 def rime_length(pho_query, pho_list):
@@ -79,13 +44,19 @@ def rime_length(pho_query, pho_list):
             break
     return rime_number
 
+
 rimes = []
 
 for mot, pho_list in phonemized_lexicon.items():
-    if rime_length(pho_query, pho_list) >= l_rime:
+    if args.riche:
+        if rime_length(pho_query, pho_list) >= 3:
+            rimes.append(mot)
+    elif args.pauvre:
+        if rime_length(pho_query, pho_list) >=1:
+            rimes.append(mot)
+    else:
+        if rime_length(pho_query, pho_list) >= args.numérique:
+            rimes.append(mot)
 
-        rimes.append(mot)
 
-pprint.pprint(rimes)
 
-#zizz
